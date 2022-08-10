18219bbc996840e901bad9aba4b72be91893b623
giuliamazzeellee <giulia.mazzilli@mail.polimi.it>
2022-08-10 13:10:07 +0200

    fixed comments and formatted Initializer.hpp

--- src/Initializer.hpp
+++ src/Initializer.hpp
@@ -1,3 +1,12 @@
+/**
+* \file Solution.hpp
+*
+* \brief Defines the class to initialize pybind11.
+*
+* \author Randeep Singh
+* \author Giulia Mazzilli
+*/
+
 #ifndef INITIALIZER_HPP_
 #define INITIALIZER_HPP_
 
@@ -6,36 +15,50 @@
 
 namespace Space4AI
 {
+/** Class to intialize pybind11. */
 class Initializer final
 {
 
-public:
+	public:
 
-  Initializer()
-  {
-    if(!already_initialized)
-    {
-      pybind11::initialize_interpreter();
-      already_initialized = true;
-    }
-    else
-    {
-      std::cout << "It's forbidden to Initialize more than once; pybind11 is already initialized" << std::endl;
-    }
-  }
+		/** Initializer class constructor.
+		*
+		*   Initializes pybind11 only if it was not initialized previously
+		*   by using the boolean member already_initialized.
+		*/
+		Initializer()
+		{
+			if(!already_initialized)
+			{
+				pybind11::initialize_interpreter();
+				already_initialized = true;
+			}
+			else
+			{
+				std::cout << "It's forbidden to Initialize more than once; pybind11 is already initialized" << std::endl;
+			}
+		}
 
-  ~Initializer()
-  {
-    if(already_initialized)
-    {
-      pybind11::finalize_interpreter();
-      already_initialized = false;
-    }
-  }
+		/** Initializer class destructor.
+		*
+		*  Finalizes pybind11 and sets already_initialized back to false.
+		*/
+		~Initializer()
+		{
+			if(already_initialized)
+			{
+				pybind11::finalize_interpreter();
+				already_initialized = false;
+			}
+		}
 
-private:
+	private:
 
-  inline static bool already_initialized = false;
+		/** Boolean variable to store the status of pybind11:
+		*   true = already initialized,
+		*   false = not initialized.
+		*/
+		inline static bool already_initialized = false;
 
 };
 
